2dba8845f6da0adfc954dad33be3c0c33e86c5b2
hamdyadam97 <hamdyadam97@gmail.com>
2024-01-23 19:21:42 +0200

    orderlist

---
+++ App.Application/Services/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using App.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Application.Services
+{
+    public interface IOrderService
+    {
+        Order GetOrderById(int id);
+        IEnumerable<Order> GetAllOrders();
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(int id);
+        public int Save();
+    }
+}
